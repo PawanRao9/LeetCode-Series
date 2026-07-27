1fa03b5cfed2ef0c0a40551626d670bb25825ee4
S PABAN <s.rameshbdk1979@gmail.com>
2026-07-27 19:04:35 +0530

    Time: 30 ms (80.49%), Space: 26.7 MB (94.81%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
@@ -5,20 +5,43 @@ class Solution(object):
         :rtype: List[int]
         """
 
+        # n = len(nums)
+        
+        # duplicate = []
+        # missing = []
+        # seen = set()
+
+        # for num in nums:
+        #     if num in seen:
+        #         duplicate.append(num)
+        #     else:
+        #         seen.add(num)
+        
+        # for i in range(1,n+1):
+        #     if i not in seen:
+        #         missing.append(i)
+
+        # return missing
+
         n = len(nums)
-        
-        duplicate = []
-        missing = []
-        seen = set()
 
-        for num in nums:
-            if num in seen:
-                duplicate.append(num)
-            else:
-                seen.add(num)
+        # freq = [0] *(n + 1)
+        # for num in nums:
+        #     freq[num] +=1
         
-        for i in range(1,n+1):
-            if i not in seen:
-                missing.append(i)
+        # duplicte = []
+        # missing = []
 
-        return missing
+        # for i in range(1,n+1):
+        #     if freq[i] == 0:
+        #         missing.append(i)
+        #     # elif freq[i] > 1:
+        #     #     duplicate.append(i)
+        # return missing
+
+        num_set = set(nums)
+        ans = []
+        for i in range(1, n+1):
+            if i not in num_set:
+                ans.append(i)
+        return ans
