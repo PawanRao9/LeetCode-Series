8bb32a0058a365a791379d9330a072c1a4e78f03
S PABAN <s.rameshbdk1979@gmail.com>
2026-08-04 19:02:47 +0530

    Time: 3 ms (85.48%), Space: 12.3 MB (98.2%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
@@ -4,12 +4,31 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        n = len(nums)
-        new_arr = []
-        for i in range(0,n):
-            count = 0
-            for j in range(0,n):
-                if (nums[i]>nums[j]):
-                    count += 1
-            new_arr.append(count)
-        return new_arr
+        # n = len(nums)
+        # new_arr = []
+        # for i in range(0,n):
+        #     count = 0
+        #     for j in range(0,n):
+        #         if (nums[i]>nums[j]):
+        #             count += 1
+        #     new_arr.append(count)
+        # return new_arr
+        freq = [0] * 101
+
+        # Count frequencies
+        for num in nums:
+            freq[num] += 1
+
+        # Prefix sum
+        for i in range(1, 101):
+            freq[i] += freq[i - 1]
+
+        ans = []
+
+        # Build answer
+        for num in nums:
+            if num == 0:
+                ans.append(0)
+            else:
+                ans.append(freq[num - 1])
+        return ans
