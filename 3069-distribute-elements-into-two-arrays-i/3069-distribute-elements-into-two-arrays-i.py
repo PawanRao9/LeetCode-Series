596a0bd35c79c208ade726ac28e7496eade264ce
S PABAN <s.rameshbdk1979@gmail.com>
2026-08-20 23:34:39 +0530

    Time: 3 ms (27.42%), Space: 12.3 MB (48.39%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.py
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.py
@@ -4,14 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        arr1 = []
-        arr2 = []
-        n = len(nums)
+        arr1 = [nums[0]]
+        arr2 = [nums[1]]
 
-        arr1.append(nums[0])
-        arr2.append(nums[1])
-
-        for i in range(2,n):
+        for i in range(2,len(nums)):
             if arr1[-1] > arr2[-1]:
                 arr1.append(nums[i])
             else:
