d09fc7ccd263caafb35d12a8335dde25ea15be70
S PABAN <s.rameshbdk1979@gmail.com>
2026-08-20 23:49:05 +0530

    Time: 0 ms (100%), Space: 12.5 MB (47%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,9 +1,5 @@
 class Solution(object):
     def lengthOfLastWord(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
         s = s.strip()
         n = len(s)
         count = 0
